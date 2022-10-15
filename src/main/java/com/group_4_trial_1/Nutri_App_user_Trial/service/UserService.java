ca5cf9ca28ac39e59d346d32365a45d17e8d3535
Pratik Kamble <pratikkambleat@gmail.com>
2022-10-15 12:58:18 +0530

    Update UserService.java
    saving user into the database !

--- src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/service/UserService.java
+++ src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/service/UserService.java
@@ -8,6 +8,7 @@ import com.group_4_trial_1.Nutri_App_user_Trial.repository.UserRepository;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -23,11 +24,27 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public List<Userdto> getByUserId(Userdto user) {
-        return userRepository.findAllById(Collections.singleton(user.getId()));
+//    public List<Userdto> getByUserId(Userdto user) {
+//        return userRepository.findAll();
+//    }
+
+    public void registerUser(Userdto user) {
+        Optional<Userdto> optionalUser = userRepository
+                                        .findByUserIdentification(user.getUserIdentification());
+        if(optionalUser.isPresent()) {
+            throw new IllegalStateException("User is already present in the Database !");
+        }
+        userRepository.save(user);
     }
 
-    public void saveUser(Userdto user) {
-        System.out.println(user);
+    public void deleteUser(Long id) {
+//        boolean exist = userRepository.existsById(id);
+        boolean exist = userRepository.findById(id).isPresent();
+
+        if (!exist) {
+            throw new IllegalStateException("User is not present in the database ! Please try with different User Id !");
+        }
+        userRepository.deleteById(id);
+
     }
 }
