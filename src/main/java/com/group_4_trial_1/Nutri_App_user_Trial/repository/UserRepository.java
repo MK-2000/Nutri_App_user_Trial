4b80ffa58453426d76550a5298f807afdba207d9
Pratik Kamble <pratikkambleat@gmail.com>
2022-10-15 12:57:55 +0530

    Update UserRepository.java
    Function for finding user based on user Identification

--- src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/repository/UserRepository.java
+++ src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/repository/UserRepository.java
@@ -2,6 +2,16 @@ package com.group_4_trial_1.Nutri_App_user_Trial.repository;
 
 import com.group_4_trial_1.Nutri_App_user_Trial.entity.Userdto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Userdto, Long> {
+
+    // Postgres query -> SELECT user from user_table WHERE user_identification = ?;
+    @Query("SELECT u FROM Userdto u WHERE u.userIdentification = ?1")
+    Optional<Userdto> findByUserIdentification(String userIdentification);
+
+    @Query("DELETE from Userdto u WHERE u.userIdentification = ?1")
+    void deleteByuserId(String userId);
 }
