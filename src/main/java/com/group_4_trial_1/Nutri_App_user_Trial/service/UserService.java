31b2545f64be2357c8270762d4674bc44e946cb3
Pratik Kamble <pratikkambleat@gmail.com>
2022-10-15 17:17:05 +0530

    Update UserService.java
    Adding Post Request for all the data Members and updating set of data

--- src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/service/UserService.java
+++ src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/service/UserService.java
@@ -1,13 +1,14 @@
 package com.group_4_trial_1.Nutri_App_user_Trial.service;
 
 import com.group_4_trial_1.Nutri_App_user_Trial.entity.Userdto;
-import com.group_4_trial_1.Nutri_App_user_Trial.entity.Userdto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.group_4_trial_1.Nutri_App_user_Trial.repository.UserRepository;
 
-import java.util.Collections;
+import javax.transaction.Transactional;
+import java.sql.Time;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -46,5 +47,56 @@ public class UserService {
         }
         userRepository.deleteById(id);
 
+    }
+
+    @Transactional
+    public void updateUser(Long id, String name, String contact, String email, String gender, String status, Float weight,
+                           Float height, String goal, Time wakeUpTime, Time sleepTime) {
+            Userdto user = userRepository.findById(id).orElseThrow(() -> new IllegalStateException(
+                    "User with this id is not present in the database."
+        ));
+            if(name != null && name.length() > 0 && !Objects.equals(name, user.getName())) {
+                user.setName(name);
+            }
+
+            if(contact != null && contact.length() == 10 && !Objects.equals(contact, user.getContact())) {
+                user.setContact(contact);
+            }
+
+            if(email != null && email.length() > 0 && email.contains("@") && email.contains(".")
+                    && Objects.equals(email, user.getEmail())) {
+                Optional<Userdto> optionalUser = userRepository.findUserByemail(email);
+                if(optionalUser.isPresent()) {
+                    throw new IllegalStateException("User with same email is already present in the database!");
+                }
+                user.setEmail(email);
+            }
+
+//            if(gender.equalsIgnoreCase("Male") || gender.equalsIgnoreCase("Female") || !gender.equals(null)){
+//                user.setGender(gender);
+//            }
+//
+//            if((status.equalsIgnoreCase("Active") || status.equalsIgnoreCase("Inactive") || !status.equals(null))
+//                                        && !Objects.equals(status, user.getStatus())) {
+//                user.setStatus(status);
+//            }
+//
+//            if(weight > 0F) {
+//                user.setWeight(weight);
+//            }
+//            if(height > 0F) {
+//                user.setHeight(height);
+//            }
+//
+//            if(goal != null && goal.length() > 0 && !Objects.equals(goal, user.getGoal())) {
+//                user.setGoal(goal);
+//            }
+//
+//            if(wakeUpTime != null) {
+//                user.setWakeUpTime(wakeUpTime);
+//            }
+//            if(sleepTime != null) {
+//                user.setSleepTime(sleepTime);
+//            }
     }
 }
