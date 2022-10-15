5f75207f88c548a26782f3eb0c9bce388c8ee98f
Pratik Kamble <pratikkambleat@gmail.com>
2022-10-15 17:16:27 +0530

    Update UserController.java
    Checking Transactional and Updating the database using Post Request

--- src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/controller/UserController.java
+++ src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.group_4_trial_1.Nutri_App_user_Trial.service.UserService;
 
+import java.sql.Time;
 import java.util.List;
 
 @RestController
@@ -32,4 +33,22 @@ public class UserController {
     public void deleteUser(@PathVariable("id") Long id) {
         userService.deleteUser(id);
     }
+
+    @PutMapping(path = "{id}")
+    public void updateUser(
+            @PathVariable("id") Long id,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String contact,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String gender,
+            @RequestParam(required = false) String status,
+            @RequestParam(required = false) Float weight,
+            @RequestParam(required = false) Float height,
+            @RequestParam(required = false) String goal,
+            @RequestParam(required = false) Time wakeUpTime,
+            @RequestParam(required = false) Time sleepTime
+            ) {
+        userService.updateUser(id, name, contact, email, gender, status, weight, height, goal, wakeUpTime, sleepTime);
+    }
+
 }
