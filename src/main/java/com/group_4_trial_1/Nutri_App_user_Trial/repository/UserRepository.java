9fede339e8e786b86a0dee7c3cd1b57dfc46efd8
Pratik Kamble <pratikkambleat@gmail.com>
2022-10-16 11:20:09 +0530

    Update UserRepository.java
    Updating Entity Package for better readability

--- src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/repository/UserRepository.java
+++ src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/repository/UserRepository.java
@@ -1,20 +1,20 @@
 package com.group_4_trial_1.Nutri_App_user_Trial.repository;
 
-import com.group_4_trial_1.Nutri_App_user_Trial.entity.Userdto;
+import com.group_4_trial_1.Nutri_App_user_Trial.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<Userdto, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     // Postgres query -> SELECT user from user_table WHERE user_identification = ?;
-    @Query("SELECT u FROM Userdto u WHERE u.userIdentification = ?1")
-    Optional<Userdto> findByUserIdentification(String userIdentification);
+    @Query("SELECT u FROM User u WHERE u.userIdentification = ?1")
+    Optional<User> findByUserIdentification(String userIdentification);
 
 //    @Query("DELETE from Userdto u WHERE u.userIdentification = ?1")
 //    void deleteByuserId(String userId);
 
-    @Query("SELECT u FROM Userdto u WHERE u.email = ?1")
-    Optional<Userdto> findUserByemail(String email);
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    Optional<User> findUserByemail(String email);
 }
